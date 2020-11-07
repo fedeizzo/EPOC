1a544a3f9a5bd024d3952e29996c64c3d8a386fc
Federico Izzo <federico.izzo99@gmail.com>
2020-11-07 09:51:44 +0100

    Add user model, create-user script

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,20 +1,35 @@
-// import { hashPassword } from '@foal/core';
+import { hashPassword } from '@foal/core';
 import { model, models, Schema } from 'mongoose';
 
 const userSchema: Schema = new Schema({
-  // email: {
-  //   required: true,
-  //   type: String,
-  //   unique: true
-  // },
-  // password: {
-  //   required: true,
-  //   type: String,
-  // }
+  firstName: {
+    required: true,
+    type: String,
+    unique: false
+  },
+  secondName: {
+    required: true,
+    type: String,
+    unique: false
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true
+  },
+  username: {
+    required: true,
+    type: String,
+    unique: true
+  },
+  password: {
+    required: true,
+    type: String,
+  }
 });
 
-// userSchema.methods.setPassword = async function(password: string) {
-//   this.password = await hashPassword(password);
-// };
+userSchema.methods.setPassword = async function(password: string) {
+  this.password = await hashPassword(password);
+};
 
 export const User = models.User || model('User', userSchema);
--- src/scripts/create-user.ts
+++ src/scripts/create-user.ts
@@ -1,39 +1,49 @@
 // 3p
 import { Config } from '@foal/core';
-// import { isCommon } from '@foal/password';
 import { connect, disconnect } from 'mongoose';
 
 // App
-import { User } from '../app/models';
+import { User } from '../app/models'
 
 export const schema = {
   additionalProperties: false,
   properties: {
-    // email: { type: 'string', format: 'email' },
-    // password: { type: 'string' },
+    firstName: {
+      type: 'string'
+    },
+    secondName: {
+      type: 'string'
+    },
+    email: {
+      type: 'string'
+    },
+    username: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    }
   },
-  required: [ /* 'email', 'password' */ ],
+  required: ['firstName', 'secondName', 'email', 'username', 'password'],
+  unique: ['email', 'username'],
   type: 'object',
 };
 
-export async function main(/*args*/) {
-  const user = new User();
-  // user.email = args.email;
-  // if (await isCommon(args.password)) {
-  //   console.log('This password is too common. Please choose another one.');
-  //   return;
-  // }
-  // await user.setPassword(args.password);
-
+export async function main(args: any) {
   const uri = Config.getOrThrow('mongodb.uri', 'string');
   await connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
+  const user = new User();
+  user.firstName = args.firstName;
+  user.secondName = args.secondName;
+  user.email = args.email;
+  user.username = args.username;
+  await user.setPassword(args.password);
+
   try {
-    console.log(
-      await user.save()
-    );
+    console.log(await user.save())
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
   } finally {
     await disconnect();
   }
