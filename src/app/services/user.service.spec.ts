84f44c5604cc829dfe9bfcd528f5be848a7703c1
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2020-11-11 00:48:31 +0100

    Fix user service tests, drop user script :hammer_and_wrench:

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,56 +1,66 @@
-import { createService } from '@foal/core';
+import { Config, createService } from '@foal/core';
 import { strictEqual } from 'assert';
+import { connect, connection, disconnect } from 'mongoose';
 import { UserService } from './user.service';
 
 describe("The User Service", () => {
   it('should insert correctly a user in the db', async () => {
     const service = createService(UserService);
-    const expectedReturnCode = 0;
+    const expectedReturnCode = 200;
     const actualReturnCode = await service.insertUser(
       'Mario',
-      'mario@rossi.it',
-      "mario123",
+      'mario@rossi32.it',
+      "mario12332",
       "sfcseckjnweuhc234325"
     );
 
-    strictEqual(actualReturnCode, expectedReturnCode);
+    strictEqual(actualReturnCode.code, expectedReturnCode);
   });
 
+  // Note: we can't drop the 
   it('should deny inserting a user with a common password', async () => {
     const service = createService(UserService);
-    const expectedReturnCode = 1;
+    const expectedReturnCode = 300;
     const actualReturnCode = await service.insertUser(
       'Mario',
-      'mario@rossi.it',
-      "mario123",
+      'mario@rossis.it',
+      "mario123ds",
       "password"
     );
 
-    strictEqual(actualReturnCode, expectedReturnCode);
+    strictEqual(actualReturnCode.code, expectedReturnCode);
   });
 
   it('should deny inserting two user with the same username', async () => {
     const service = createService(UserService);
-    const expectedReturnCodeFirstUser = 0;
-    const expectedReturnCodeSecondUser = 2;
+    const expectedReturnCodeFirstUser = 200;
+    const expectedReturnCodeSecondUser = 301;
 
     const actualReturnCodeFirstUser = await service.insertUser(
       'Mario',
       'mario@rossi.it',
       "mario123",
-      "sfcseckjnweuhc234325"
+      "sfcseckjnweuhc2343f25"
     );
 
     const actualReturnCodeSecondUser = await service.insertUser(
-      'Luigi',
+      'Mario',
       'mario@rossi.it',
       "mario123",
-      "fakjdhaefhvg3846234079"
+      "fakjdhaefhvg384623407v9"
     );
 
-    // strictEqual(actualReturnCodeFirstUser, expectedReturnCodeFirstUser);
-    strictEqual(actualReturnCodeSecondUser, expectedReturnCodeSecondUser);
+    strictEqual(actualReturnCodeFirstUser.code, expectedReturnCodeFirstUser);
+    strictEqual(actualReturnCodeSecondUser.code, expectedReturnCodeSecondUser);
   });
 
+  after(async () => {
+    const uri = Config.getOrThrow('mongodb.uri', 'string');
+    await connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+    await connection.db.dropCollection('userclasses');
+    let a;
+    await disconnect();
+  });
 });
 
