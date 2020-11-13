364a1135004a381b5b8019a96641901a1080d230
Federico Izzo <federico.izzo99@gmail.com>
2020-11-13 16:30:51 +0100

    add logout UI, endpoint. Try to make some controller test :art: :costruction:

--- frontend/src/authentication.ts
+++ frontend/src/authentication.ts
@@ -3,17 +3,22 @@ interface LoginData {
   password: string;
 };
 
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  return parts[1]
+}
+
 async function fetchLogin() {
   const url: string = 'http://localhost:3001/login'
   const headers = {
     'Content-type': 'application/json'
   };
   const data: LoginData = { username: 'empty', password: 'empty' };
-  // const token = document.cookie.split(";")[2].split("=")[1];
-  const token = null;
+  const token = getCookie('JWT');
 
-  console.log(token)
-  if (token !== null) {
+  console.log("---", token, "----")
+  if (token !== null && token !== undefined && token !== '') {
     headers['Authorization'] = 'Bearer ' + token;
   } else {
     const username = (<HTMLInputElement>document.getElementById('username')).value;
@@ -36,5 +41,30 @@ async function fetchLogin() {
     const passwordField = (<HTMLInputElement>document.getElementById('password'));
     passwordField.value = '';
     passwordField.placeholder = 'Wrong password';
+  }
+}
+
+async function fetchLogout() {
+  const url: string = 'http://localhost:3001/logout'
+  const headers = {
+    'Content-type': 'application/json'
+  };
+
+  const token = getCookie('JWT');
+
+  if (token !== null && token !== undefined) {
+    headers['Authorization'] = 'Bearer ' + token;
+  }
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'same-origin',
+    cache: 'no-cache',
+    redirect: 'follow',
+    credentials: 'same-origin',
+    headers,
+  });
+  if (response.status == 200) {
+    window.location.replace(response.url);
+  } else {
   }
 }
