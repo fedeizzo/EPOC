ccec2f0432e5eead38d76c2e62c07bd00664631c
civts <giacomo.zanolli@studenti.unitn.it>
2020-11-17 12:38:02 +0100

    Make RecipeService perform regex search when few matches happen 🚛

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,21 +1,19 @@
 // 3p
 import { Config } from "@foal/core";
 import { DocumentType } from "@typegoose/typegoose";
-// import {  } from 'mongoose';
 
 // App
 import { ServiceResponse } from ".";
 import { Recipe, RecipeClass } from "../models/recipe.model";
 
-
-function getShortInfo(docRecipe){
+function getShortInfo(docRecipe) {
   return {
     name: docRecipe.name,
     image: docRecipe.image,
     averageRating: docRecipe.averageRating,
     description: docRecipe.description,
-    id: docRecipe._id
-  }
+    id: docRecipe._id,
+  };
 }
 
 enum RecipeResponseType {
@@ -29,14 +27,10 @@ export class RecipeResponse implements ServiceResponse {
   public type: RecipeResponseType;
   public prop?: RecipeClass | DocumentType<RecipeClass>[];
 
-  setValuesComplete = (
-    code: number,
-    text: string,
-    prop?: RecipeClass
-  ) => {
+  setValuesComplete = (code: number, text: string, prop?: RecipeClass) => {
     this.code = code;
     this.text = text;
-    prop? this.prop = prop : this.prop = {} as RecipeClass ;
+    prop ? (this.prop = prop) : (this.prop = {} as RecipeClass);
     this.type = RecipeResponseType.complete;
   };
 
@@ -47,7 +41,7 @@ export class RecipeResponse implements ServiceResponse {
   ) => {
     this.code = code;
     this.text = text;
-    prop ?  this.prop = prop : this.prop = [];
+    prop ? (this.prop = prop) : (this.prop = []);
     this.type = RecipeResponseType.shortList;
   };
 
@@ -56,7 +50,11 @@ export class RecipeResponse implements ServiceResponse {
     return {
       code: this.code,
       text: this.text,
-      recipes: this.prop ? (this.prop as DocumentType<RecipeClass>[]).map((rec)  => getShortInfo(rec)): "",
+      recipes: this.prop
+        ? (this.prop as DocumentType<RecipeClass>[]).map((rec) =>
+            getShortInfo(rec)
+          )
+        : "",
     };
   };
 
@@ -64,35 +62,71 @@ export class RecipeResponse implements ServiceResponse {
     return {
       code: this.code,
       text: this.text,
-      userInfo: this.prop ? (this.prop as RecipeClass).getCompleteInfo() : "",
+      recipe: this.prop ? (this.prop as RecipeClass).getCompleteInfo() : "",
     };
   };
 
   buildResponse = () => {
-    return this.type == RecipeResponseType.shortList ? this.buildResponsePartialList() : this.buildResponseComplete(); 
+    return this.type == RecipeResponseType.shortList
+      ? this.buildResponsePartialList()
+      : this.buildResponseComplete();
   };
 }
 
+class ErrorWrapper {
+  constructor(error: any) {
+    this.error = error;
+  }
+  error: any;
+}
+
 export class RecipeService {
-  private uri: string = Config.getOrThrow("mongodb.uri", "string");
+  private static fieldsToSelect = "name image description averageRating";
+  private static queryLimit = 50;
 
   async getPartialRecipeList(searchString: string): Promise<RecipeResponse> {
     const response: RecipeResponse = new RecipeResponse();
-
-    // TODO: set as search in array
-    // const query = await Recipe.find({ keywords: searchString }) // Search in array
-    const query = await Recipe.find({ keywords: new RegExp(searchString) }) // Search in string
-      .select("name image description averageRating")
-      .limit(50)
-      .exec()
-      .then((result) => {
-        response.setValuesList(200, "All ok", result);
-      })
-      .catch((error) => {
-        response.setValuesList(500, error);
-      });
-      return response;  
+    let result = await this.searchExactMatches(searchString);
+    if (
+      !(result instanceof ErrorWrapper) &&
+      result.length < RecipeService.queryLimit
+    ) {
+      const moreResults = await this.searchRemainingWithRegex(
+        searchString,
+        RecipeService.queryLimit - result.length
+      );
+      result = result.concat(moreResults);
     }
+    if (result instanceof ErrorWrapper) {
+      response.setValuesList(500, result.error);
+    } else {
+      response.setValuesList(200, "All ok", result);
+    }
+    return response;
+  }
+
+  private async searchRemainingWithRegex(
+    searchString: string,
+    remaining: number
+  ) {
+    return await Recipe.find({
+      keywords: new RegExp(searchString),
+    })
+      .select(RecipeService.fieldsToSelect)
+      .limit(remaining)
+      .exec()
+      .catch((_) => []);
+  }
+
+  private async searchExactMatches(searchString: string) {
+    return await Recipe.find({
+      keywords: searchString,
+    })
+      .select(RecipeService.fieldsToSelect)
+      .limit(RecipeService.queryLimit)
+      .exec()
+      .catch((e) => new ErrorWrapper(e));
+  }
 
   async getCompleteRecipe(recipeId): Promise<RecipeResponse> {
     let response: RecipeResponse = new RecipeResponse();
@@ -100,7 +134,7 @@ export class RecipeService {
     const query = await Recipe.findById(recipeId)
       .exec()
       .then((result) => {
-        if(result == null){
+        if (result == null) {
           response.setValuesComplete(404, "Recipe not found");
         } else {
           response.setValuesComplete(200, "All ok", result as RecipeClass);
