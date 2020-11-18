8466b0990a404e263131fbd65921cd0d6c06d93a
Francesco Bozzo <francesco.bozzo.99@gmail.com>
2020-11-18 22:00:02 +0100

    Use random normal distribution when generating plan :man_teacher:

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -4,24 +4,40 @@ import { RecipeClass } from '../models/recipe.model';
 import { RecipeService } from '../services';
 import { DocumentType } from '@typegoose/typegoose';
 import { CostLevels } from '../models/recipe.model';
+import { normal } from 'random';
 
 export class PlanService {
+  private alpha: number = 1.5; // multiplicative factor for normal std
   private recipeService: RecipeService = new RecipeService();
-  // TODO when preferences will be implemented change this line
-  getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+
+  getRandomArbitrary(min: number, max: number): number {
+    // return Math.floor(Math.random() * (max - min) + min);
+    const normalDistribution: Function = normal(0, Math.sqrt(max) * this.alpha);
+    const randomValue: number = Math.floor(Math.abs(normalDistribution()));
+    return randomValue > max ? max : randomValue;
   }
 
-  async getPlan(numberOfRecipes: number, budget?: CostLevels, preferences?: any, user?: DocumentType<UserClass>) {
-    const obj = {};
+  // TODO when preferences will be implemented change this line
+  async getPlan(numberOfRecipes: number, budget?: CostLevels, preferences?: any, user?: DocumentType<UserClass>)
+    : Promise<DocumentType<RecipeClass>[]> {
+    const queryParams = {};
     if (budget !== undefined) {
-      obj['estimatedCost'] = budget;
+      queryParams['estimatedCost'] = budget;
     }
-    const recipes = await this.recipeService.findExactMatches(obj);
+    const recipes = await this.recipeService.findExactMatches(queryParams);
+
     const selectedRecipes: DocumentType<RecipeClass>[] = [];
-    for (let i = 0; i < numberOfRecipes; i++) {
-      selectedRecipes.push(recipes[this.getRandomArbitrary(0, (recipes as DocumentType<RecipeClass>[]).length - 1)]);
+    var setRandomIndexes: Set<number> = new Set<number>();
+
+    // avoid repeated random generated numbers
+    while (setRandomIndexes.size < numberOfRecipes) {
+      setRandomIndexes.add(this.getRandomArbitrary(0, (recipes as DocumentType<RecipeClass>[]).length - 1));
     }
+
+    for (let i of setRandomIndexes) {
+      selectedRecipes.push(recipes[i]);
+    }
+
     return selectedRecipes;
   }
 }
