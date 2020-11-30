b52715e61470e75eecc0e732127688143ef1f3d0
civts <giacomo.zanolli@studenti.unitn.it>
2020-11-30 14:37:58 +0100

    User can add preferences 🍔

--- frontend/src/pages/preferences.ts
+++ frontend/src/pages/preferences.ts
@@ -1,9 +1,8 @@
+const token = getJwtCookie("JWT");
+
 //Retrieves user's preferences from the server
-async function fetchPrefs() {
-  const token = getJwtCookie("JWT");
-  const headers = {};
+async function getUserPreferences() {
   if (token != undefined) {
-    headers["Authorization"] = "Bearer " + token;
     // const res = await fetch(`/api/v1/preferenes`); const prefs = (await res.json()).content;
     const prefs = {
       positive: {
@@ -86,10 +85,58 @@ function listToDivs(
   const container = document.createElement("div");
   container.className = "preferences_list";
   for (const i of prefs) {
+    const d = getSinglePrefDiv(i);
+    container.append(d);
+  }
+  const div = document.createElement("div");
+  const input = document.createElement("input") as HTMLInputElement;
+  input.placeholder = "add";
+  const addPreference = async () => {
+    const preference = input.value;
+    if (preference.length > 0) {
+      const body = {
+        category: listName,
+        content: preference,
+      };
+      const res = await fetch(
+        `/api/v1/preference/${positive ? "positive" : "negative"}`,
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (res.status === 200) {
+        const last = container.children[container.children.length - 1];
+        input.value = "";
+        container.removeChild(last);
+        container.append(getSinglePrefDiv(preference), last);
+      } else {
+        alert("Preference could not be added");
+      }
+    }
+  };
+  input.onkeypress = (e) => {
+    if (e.code === "Enter") {
+      addPreference();
+    }
+  };
+  const add = document.createElement("div");
+  div.classList.add("preference", "card", "clickable");
+  add.innerText = "+";
+  add.onclick = addPreference;
+  div.append(input, add);
+  container.appendChild(div);
+  return container;
+
+  function getSinglePrefDiv(i: String) {
     const d = document.createElement("div");
     d.innerText = i.valueOf();
     d.classList.add("preference", "card");
     const deleteButton = document.createElement("div");
+    deleteButton.className = "clickable";
     deleteButton.innerHTML =
       '<svg width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6L6 18M6 6l12 12"/></svg>';
     d.appendChild(deleteButton);
@@ -99,9 +146,8 @@ function listToDivs(
         d.remove();
       }
     };
-    container.append(d);
+    return d;
   }
-  return container;
 }
 
 async function deletePreference(
@@ -109,12 +155,10 @@ async function deletePreference(
   listName: String,
   positive: boolean
 ) {
-  const body = [
-    {
-      category: listName,
-      content: preference,
-    },
-  ];
+  const body = {
+    category: listName,
+    content: preference,
+  };
   const res = await fetch(
     `/api/v1/preference/${positive ? "positive" : "negative"}`,
     {
@@ -140,4 +184,4 @@ enum CostLevels {
   veryHigh = "molto elevata",
 }
 
-fetchPrefs();
+getUserPreferences();
