a7e4a50218250cf6215cf27bf445838375ab0aa7
Federico Izzo <federico.izzo99@gmail.com>
2020-12-03 12:22:44 +0100

    Refactor recipe list html page :100:

--- frontend/src/pages/search.ts
+++ frontend/src/pages/search.ts
@@ -1,4 +1,4 @@
-document.getElementById("searchBar")!.onkeydown = function (e) {
+document.getElementById("searchBar")!.onkeydown = function(e) {
   if (e.code === "Enter") {
     const element = <HTMLInputElement>document.getElementById("searchBar");
     const query = encodeURIComponent(element.value);
@@ -17,25 +17,37 @@ async function search(query: string) {
   const j = await response.json();
   const recipes: Partial<Recipe>[] = j["recipes"];
   const elements = recipes.map((r) => recipeCard(r));
-  const container = document.createElement("div");
-  container.className = "recipeContainer";
-  for (const e of elements) {
-    container.appendChild(e);
+  console.log('elements: ', elements)
+  const container = document.getElementById("recipeContainer");
+  if (container) {
+    for (const e of elements) {
+      container.appendChild(e);
+    }
+    const a = document.getElementsByClassName('recipeContainer');
+    for (let i = 0; i < a.length; i++) {
+      a[i].remove();
+      document.getElementsByTagName("body")[0].appendChild(container);
+    }
   }
-  const a = document.getElementsByClassName('recipeContainer');
-  for (let i = 0; i < a.length; i++) {
-    a[i].remove();
-  }
-  document.getElementsByTagName("body")[0].appendChild(container);
 
+
+  // <li class="media item-list">
+  //   <img src="https://www.giallozafferano.it/images/ricette/223/22324/foto_hd/hd650x433_wm.jpg" class="mr-3 rounded" alt="...">
+  //   <div class="media-body">
+  //     <h3 class="mt-0 mb-1">Nome della ricetta</h3>
+  //     Descrizione Descrizione Descrizione Descrizione Descrizione Descrizione Descrizione Descrizione Descrizione Descrizione
+  //   </div>
+  // </li>
   function recipeCard(r: Partial<Recipe>): HTMLElement {
-    const card = document.createElement("div");
-    card.className = "recipe_card";
+    const card = document.createElement("li");
+    card.className = "media item-list";
     const image = <HTMLImageElement>document.createElement("img");
     image.src = r.image ?? "https://via.placeholder.com/250";
+    image.className = "mr-3 rounded"
     image.setAttribute("src", r.image ?? "https://via.placeholder.com/250");
     const central = document.createElement("div");
-    central.innerHTML = `<h1>${r.name ?? ""}</h1><p>${r.description ?? ""}</p>`;
+    central.className = "media-body";
+    central.innerHTML = `<h3 class="mt-0 mb-1">${r.name ?? ""}</h3><p>${r.description ?? ""}</p>`;
     card.appendChild(image);
     card.appendChild(central);
     card.onclick = () => {
